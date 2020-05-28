c323fdf93778fe8ba91335d0b844d041d984d8e4
Avinesh Kumar Singh <aviksingh04@gmail.com>
2020-05-28 11:32:07 +0530

    All Desiging issue resolve

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, StatusBar, View, Platform } from 'react-native';
 import MainNavigator from './src/navigation/MainNavigator';
 // import Homepage from './src/Homepage/homepage'
-// import Recieve from './src/Recieved/recieve'
+import Recieve from './src/Recieved/recieve'
 // import Splash from './src/Splash/splash'
 // import Slider from './src/slider/slider'
 import AddAccount from './src/AddAccount/AddAccount'
