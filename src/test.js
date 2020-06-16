92d03b4ce0bdf74f16f1df9b179c577ca44b5402
aviksingh04 <aviksingh04@gmail.com>
2020-06-17 05:21:42 +0530

    create a dummy class test

---
+++ src/test.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native'
+
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <View>
+                <Text>Hello , Test Class</Text>
+            </View>
+        );
+    }
+}
+
+export default Test;
