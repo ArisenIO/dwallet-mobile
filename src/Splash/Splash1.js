21c433b6e2a3bcde67f8fe21b997cbc8aed6c17d
gaurav563 <563gauravshakya@gmail.com>
2020-06-24 00:21:54 +0530

    fade out fade in added in both  splash

--- src/Splash/Splash1.js
+++ src/Splash/Splash1.js
@@ -1,32 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import {View, Text, TouchableOpacity, Image,StyleSheet} from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, Animated } from 'react-native'
 import Icon from '../assets/Icon'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 class Splash1 extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-
-         };
+        this.fadeAnim = new Animated.Value(1);
+        console.disableYellowBox = true;
     }
     async componentDidMount() {
-       
-        setTimeout(() => {
-            //Actions.AppIntro();
-// alert("ok")
-Actions.replace('Splash')
-        }, 1000);
+
+        Animated.timing(          // Animate over time
+            this.fadeAnim, // The animated value to drive
+            {
+              toValue: 0,           // Animate to opacity: 1 (opaque)
+              duration: 2000,
+            }
+          ).start(() => {
+            console.log('fading out');
+            Actions.replace('Splash')
+          });
+      
+
+        // setTimeout(() => {
+        //     Actions.replace('Splash')
+        // }, 1000);
     }
     render() {
         return (
             <View style={styles.container}>
-                <Image
-                style={{width:wp('100%'), height:hp('110%')}}
-                resizeMode="contain"
-                source={Icon.frist_splash}
-                />  
+                <Animated.View style={{ ...this.props.style, opacity: this.fadeAnim }} >
+                    <Image
+                        style={{ width: wp('100%'), height: hp('110%') }}
+                        resizeMode="contain"
+                        source={Icon.frist_splash}
+                    />
+                </Animated.View>
+
             </View>
         );
     }
@@ -35,7 +47,7 @@ Actions.replace('Splash')
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
+
     },
 });
 export default Splash1;
--- src/Splash/splash.js
+++ src/Splash/splash.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import {
-    StyleSheet,
-    Animated,
-    View, Text,Image, ImageBackground, Platform
-} from "react-native";
+import { StyleSheet, Animated, View, Text, Image, ImageBackground, Platform } from "react-native";
 import AsyncStorage from '@react-native-community/async-storage';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Icon from '../assets/Icon'
@@ -16,11 +12,23 @@ export default class Splash extends Component {
             restoredone: '',
             WalletCreated: '',
             Created: '',
-            from_restore: ''
+            from_restore: '',
         };
+        this.fadeAnim = new Animated.Value(0);
+
         console.disableYellowBox = true;
     }
     async componentDidMount() {
+        Animated.timing(          // Animate over time
+            this.fadeAnim, // The animated value to drive
+            {
+                toValue: 1,           // Animate to opacity: 1 (opaque)
+                duration: 2000,
+            }
+        ).start(() => {
+            Actions.replace('Splash')
+        });
+
         await AsyncStorage.getItem("RestoreDone").then((x2) => {
             if (x2) {
                 this.state.restoredone = x2
@@ -43,48 +51,34 @@ export default class Splash extends Component {
         })
         setTimeout(() => {
             //Actions.AppIntro();
-
             console.log("inside set timeout");
-
-            AsyncStorage.getItem('items').then((value) =>{
-
-                if(value){
+            AsyncStorage.getItem('items').then((value) => {
+                if (value) {
                     Actions.replace('Create_Pin')
                 }
-                else{
+                else {
                     Actions.replace('AppIntro');
                 }
-             }).catch((errr)=>{
-                console.log("error in retri",errr);
-    
-             });
+            }).catch((errr) => {
+                console.log("error in retri", errr);
+            });
 
         }, 2000);
-        //     console.log("inside set timeout");
 
-        //     AsyncStorage.getItem('creds')homepage.then((value) =>{
-
-        //         if(value){
-        //             Actions.homepage()
-        //         }
-        //         else{
-        //             Actions.AppIntro();
-        //         }
-        //      }).catch((errr)=>{
-        //         console.log("error in retri",errr);
-    
-        //      });
-        // }, 3000);
     }
     render() {
         return (
             <View style={styles.container}>
-                <Image
-                style={{width:wp('100%'), height:hp('110%')}}
-                resizeMode="contain"
-                source={Icon.Splash_Icon}
-                />            
-                  {/* <View style={{ width: wp('50%'), height: hp('50%'), justifyContent: 'center', alignItems: 'center' }}>
+                <Animated.View style={{ ...this.props.style, opacity: this.fadeAnim }} >
+
+                    <Image
+                        style={{ width: wp('100%'), height: hp('110%') }}
+                        resizeMode="contain"
+                        source={Icon.Splash_Icon}
+                    />
+                </Animated.View>
+
+                {/* <View style={{ width: wp('50%'), height: hp('50%'), justifyContent: 'center', alignItems: 'center' }}>
                    <Image
                    style={{ width: wp('50%'), height: hp('30%'), }}
                    source={Icon.App_logo1}
@@ -108,6 +102,6 @@ export default class Splash extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
+
     },
 });
